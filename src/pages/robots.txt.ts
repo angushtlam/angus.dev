461035f78e79fc8b6b27f52d37e716fc45a62f18
Angus Lam <me@anguslam.com>
2026-06-21 21:50:46 -0400

    Add SEO improvements

---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,15 @@
+export function GET() {
+  return new Response(
+    [
+      "User-agent: *",
+      "Allow: /",
+      "",
+      "Sitemap: https://angus.dev/sitemap-index.xml",
+    ].join("\n"),
+    {
+      headers: {
+        "Content-Type": "text/plain; charset=utf-8",
+      },
+    }
+  );
+}
